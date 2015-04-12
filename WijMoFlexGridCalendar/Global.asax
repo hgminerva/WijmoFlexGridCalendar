25d95549b506e2894663b23e98bd2071476b1c39
hgminerva <hgminerva@gmail.com>
2015-04-12 14:50:09 -0800

    Initial Commit

---
+++ WijMoFlexGridCalendar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WijMoFlexGridCalendar.MvcApplication" Language="C#" %>
---
+++ WijMoFlexGridCalendar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WijMoFlexGridCalendar.Startup))]
+namespace WijMoFlexGridCalendar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
